d7491139f5737f9f92a60d3a8b9d06406960aafb
MDuckkk <bmd040510@gmail.com>
2026-04-24 11:01:32 +0700

    feat: add TODO quality_check

--- starter_code/quality_check.py
+++ starter_code/quality_check.py
@@ -7,9 +7,15 @@ def run_semantic_checks(doc_dict: dict) -> bool:
     
     # 1. Kiểm tra độ dài: Nếu content trống hoặc < 10 ký tự -> False
     # TODO: Thực hiện kiểm tra độ dài ở đây
+    if len(content) < 10:
+        return False
     
     # 2. Kiểm tra từ khóa lỗi
     toxic_keywords = ["Null pointer exception", "OCR Error", "Traceback"]
     # TODO: Lặp qua các từ trong toxic_keywords, nếu từ đó xuất hiện trong content -> Trả về False
+    toxic_keywords = ["Null pointer exception", "OCR Error", "Traceback"]
+    for keyword in toxic_keywords:
+        if keyword in content:
+            return False
             
     return True
